30998771f819e6889a9c9f46e04d92c7d96075ac
Kafkin <papayrus137@mail.ru>
2022-03-27 16:43:05 +0300

    feat: add carousel on main pages

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,19 @@
         </li>
       </ul>
 
-      <div class="carousel">
-        <img src="./assets/img,icon,video/MotorcycleOne.png" alt="">
+      <div id="loader" v-if="isLoading === true">
+        <div l a></div>
+        <div l b></div>
+        <div l c></div>
+        <div l d></div>
+        <div l e></div>
+        <div l f></div>
+        <div l g></div>
+        <div l h></div>
+      </div>
+
+      <div class="carousel" v-else v-cloak>
+        <vCard v-for="(item, index) in arrayMotocycle" :key="index" :arrayItem="item" @nextMotorcycle="nextMotorcycle" :class="{ 'front': index === (maxIndex - 1) }" v-show="maxIndex >= index && minIndex <= index"></vCard>
       </div>
 
       <ul class="pairs">
@@ -34,24 +45,50 @@
 </template>
 
 <script>
+import vCard from './components/v-card.vue'
+
 export default {
   name: 'App',
   data(){
     return{
-      
+      arrayMotocycle: [],
+      arrayEmpty: true,
+      isLoading: true,
+      countShowCard: 3,
+      maxIndex: 2,
+      minIndex: 0,
+      maxStandart: 2,
+      minStandart: 0
     }
   },
 
   methods:{
+    nextMotorcycle(card){
+      let index = this.arrayMotocycle.indexOf(card)
+      console.log( index );
 
+      this.maxIndex = index + 1
+      this.minIndex = index - 1
+      // this.maxIndex = this.maxStandart + (index * this.countShowCard)
+      // this.minIndex = this.minStandart + (index * this.countShowCard)
+    }
   },
   
   components: {
-
+    vCard
   },
 
   computed(){
     this.$refs.BgSmokeVideo.playbackRate = 1
+  },
+
+  mounted(){
+    fetch('http://localhost:3000/Motorcycle')
+      .then(json => json.json())
+      .then(data => {
+        this.arrayMotocycle = data
+        this.isLoading = false
+      })
   }
 }
 </script>
---
+++ src/components/v-card.vue
@@ -0,0 +1,47 @@
+<template>
+  
+  <div class="emptyCard" v-if="arrayItem.id === 'none'">
+    <div class="emptyCardImg"></div>
+  </div>
+
+  <div class="containerCard" v-else v-cloak>
+    <img :src="require(`../assets/img,icon,video/${arrayItem.img}`)" :alt="arrayItem.title" @click="$emit('nextMotorcycle', arrayItem)">
+    <div class="info">
+      <div class="title">
+        <h1>Название: </h1>
+        <h2>{{ arrayItem.title }}</h2>
+      </div>
+      <div class="price">
+        <h1>Цена: </h1>
+        <h2>{{ arrayItem.price }}</h2>
+        <h3>{{ arrayItem.currency }}</h3>
+      </div>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  },
+
+  methods:{
+    
+  },
+
+  props:{
+    arrayItem: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style>
+@import url('../assets/styles/Card.css');
+
+</style>
