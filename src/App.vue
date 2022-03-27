6a40c048022b7413cb2c7881c5107ecd605ca92f
Kafkin <papayrus137@mail.ru>
2022-03-27 11:47:11 +0300

    initial git (default files)

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  
+  <h1>empty</h1>
+
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return{
+
+    }
+  },
+
+  methods:{
+
+  },
+  
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+@import url('./assets/styles/App.css');
+
+</style>
